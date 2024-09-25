feb7ba691fc975a641cf3bb7ff04db106a41c8ed
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2024-09-25 18:17:27 -0400

    Use script to make tags

---
+++ .github/generate-data.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Generate metadata used by the actions
+
+function get_tag() {
+    NAME=$1
+    echo "type=raw,value=$NAME" >> /tmp/docker-tags.txt
+}
+
+get_tag "$REF_NAME-$TARGET"
+
+if [[ $TARGET == "base" ]]; then
+    get_tag "$REF_NAME"
+fi
+
+# Generate the tags to push to docker
+if [[ $REF == refs/tags/* ]]; then
+    get_tag "latest-$TARGET"
+    if [[ $TARGET == "base" ]]; then
+        get_tag "latest"
+    fi
+fi
+
+# Print multi-line string to github ENV
+
+echo 'DOCKER_TAGS_AUTO_GEN<<EOF' >> "$GITHUB_ENV"
+cat /tmp/docker-tags.txt >> "$GITHUB_ENV"
+echo EOF >> "$GITHUB_ENV"
